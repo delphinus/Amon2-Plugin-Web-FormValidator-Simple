5c33659ccd08675794d06a8881278ccf42506677
delphinus35 <delphinus@remora.cx>
2012-09-01 09:51:58 +0900

    initial commit

---
+++ lib/Amon2/Plugin/Web/FormValidator/Simple.pm
@@ -0,0 +1,32 @@
+package Amon2::Plugin::Web::FormValidator::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Amon2::Plugin::Web::FormValidator::Simple -
+
+=head1 SYNOPSIS
+
+  use Amon2::Plugin::Web::FormValidator::Simple;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Web::FormValidator::Simple is
+
+=head1 AUTHOR
+
+JINNOUCHI Yasushi E<lt>delphinus@remora.cxE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
